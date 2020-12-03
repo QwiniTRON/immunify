6b867331ce001b1dd1e71d6656b5c0cc66c97b3d
QwiniTRON <barabanzz871@gmail.com>
2020-12-03 14:42:10 +0500

    pwa ready

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './styles/index.scss';
 import {mainTheme} from './styles/Theme';
@@ -19,7 +20,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.register();
 reportWebVitals();
