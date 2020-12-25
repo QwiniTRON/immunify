1714cf130f841819034ec77358c54e8fca296d1a
Zinevich Yan <facecrack1337@gmail.com>
2020-12-25 19:41:20 +0500

    + Show patient vacciens

--- src/App.tsx
+++ src/App.tsx
@@ -123,6 +123,10 @@ const App: React.FC<AppProps> = function ({
                 <Region />
               </Route>
 
+              <Route path="/test" exact>
+                <Vaccination />
+              </Route>
+
 
               <Redirect to="/profile" />
 
